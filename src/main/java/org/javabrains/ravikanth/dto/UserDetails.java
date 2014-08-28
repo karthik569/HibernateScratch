2a8e1da1bd7258645c62ca5300e697f9583bf708
Ravikanth <ravikanthreddy89@gmail.com>
2014-08-28 15:21:14 -0700

    hbm2ddl and named configuration.

--- src/main/java/org/javabrains/ravikanth/dto/UserDetails.java
+++ src/main/java/org/javabrains/ravikanth/dto/UserDetails.java
@@ -1,13 +1,17 @@
 package org.javabrains.ravikanth.dto;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-@Entity
+@Entity(name="USER_DETAILS")
 public class UserDetails {
 	
 	@Id
+	@Column(name="USER_ID")
 	private int userId;
+	
+	@Column(name="USER_NAME")
 	private String userName;
 	
 	public UserDetails(){
