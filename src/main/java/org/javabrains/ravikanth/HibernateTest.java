b60f710fad37045e2c56e41f0aae0687fd62d29f
Ravikanth <ravikanthreddy89@gmail.com>
2014-08-28 14:29:07 -0700

    Initial Commit: Ep1-3

---
+++ src/main/java/org/javabrains/ravikanth/HibernateTest.java
@@ -0,0 +1,37 @@
+package org.javabrains.ravikanth;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.javabrains.ravikanth.dto.UserDetails;
+
+public class HibernateTest {
+
+	public static void main(String [] args){
+		
+		// Build Session factory from the Xml configuration file
+		SessionFactory sf=new Configuration().configure().buildSessionFactory();
+	
+		//Open a session
+		Session session=sf.openSession();
+	
+		//Create the model object to be saved
+		UserDetails userDetails=new UserDetails();
+		userDetails.setUserId(1);
+		userDetails.setUserName("Ravikanth");
+		
+		//begin the transaction
+		session.beginTransaction();
+		
+		//save the object
+		session.save(userDetails);
+
+		//commit the transaction
+		session.getTransaction().commit();
+		
+		//release the resources;
+		session.close();
+		sf.close();
+		
+	}
+}
---
+++ src/main/java/org/javabrains/ravikanth/dto/UserDetails.java
@@ -0,0 +1,31 @@
+package org.javabrains.ravikanth.dto;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class UserDetails {
+	
+	@Id
+	private int userId;
+	private String userName;
+	
+	public UserDetails(){
+		
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	
+
+}
