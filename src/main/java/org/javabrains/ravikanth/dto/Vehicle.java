a50316d1723e87e8ce7aee7b833ff002c535a2f0
ravikanth <ravikanthreddy89@gmail.com>
2014-09-01 14:31:44 -0700

    Ep19 : Table per class.

--- src/main/java/org/javabrains/ravikanth/dto/Vehicle.java
+++ src/main/java/org/javabrains/ravikanth/dto/Vehicle.java
@@ -10,12 +10,11 @@ import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 
 @Entity
-@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name="VEHICLE_TYPE", discriminatorType=DiscriminatorType.STRING)
+@Inheritance(strategy=InheritanceType.TABLE_PER_CLASS)
 public class Vehicle {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.TABLE)
 	private int vehicleId;
 	private String vehicleName;
 	
