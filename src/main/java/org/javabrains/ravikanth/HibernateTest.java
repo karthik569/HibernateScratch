a88a2128dde3d047ce44dd6b853fb48ee21e98c8
ravikanth <ravikanthreddy89@gmail.com>
2014-09-02 13:03:30 -0700

    Ep31 : Projects and query using Example.

--- src/main/java/org/javabrains/ravikanth/HibernateTest.java
+++ src/main/java/org/javabrains/ravikanth/HibernateTest.java
@@ -7,6 +7,10 @@ import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Example;
+import org.hibernate.criterion.Projection;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Property;
 import org.hibernate.criterion.Restrictions;
 import org.javabrains.ravikanth.dto.UserDetails;
 
@@ -20,19 +24,44 @@ public class HibernateTest {
 		session.getTransaction().begin();
 		
 		//create a criteria object and add restrictions
-		Criteria criteria=session.createCriteria(UserDetails.class);
-//		criteria.add(Restrictions.eq("userName", "User 10"))
-//				 .add(Restrictions.gt("userId", 5));
-//		
+		/*Example 1 using criteria and projection*/
+		Criteria criteria=session.createCriteria(UserDetails.class)
+				                 .setProjection(Projections.property("userName"));
 		
-		criteria.add(Restrictions.or(Restrictions.between("userId", 1, 3), Restrictions.between("userId", 5, 10)));
-		List<UserDetails> users=(List<UserDetails>)criteria.list();
+		List<String> users=(List<String>)criteria.list();
+		
+		/*Example 2 using criteria and prjection */
+		Criteria c2=session.createCriteria(UserDetails.class)
+						   .setProjection(Projections.property("userId"));
+		List<Integer> userIds =(List<Integer>)c2.list();
+		
+		/*Example using "Example" object */		
+		UserDetails exampleUser=new UserDetails();
+		exampleUser.setUserName("User 10");
+		Example example =Example.create(exampleUser);
+		Criteria c3= session.createCriteria(UserDetails.class)
+							.add(example);
+		
+		List<UserDetails> uD=(List<UserDetails>)c3.list();
+		
+		
+		
 		session.getTransaction().commit();
 		session.close();
 		sf.close();
 			
 		System.out.println("Number of users in the table : "+users.size());
 		
-		for(UserDetails user : users) System.out.println(" Name  : "+user.getUserName());
+		
+		System.out.println("Projection result of usernames");
+		for(String user : users) System.out.println(" Name  : "+user);
+		
+		
+		System.out.println("Projection result of userIds");
+		for(Integer id : userIds) System.out.println("Id : "+id);
+		
+		
+		System.out.println("Result using Example object");
+		System.out.println("Result size : "+uD.size());
 	}
 }
